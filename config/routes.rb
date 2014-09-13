e33d6604776782d0c0604966f806f7706e01fb07
Ricard Sole <ricard.sole@xing.com>
2014-09-13 18:40:08 +0200

    fixing bad files

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  get '/search' => 'tweets#search'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
