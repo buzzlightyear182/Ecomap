ae127d96034a906f3013c8c0203ac4108d3dc58f
SirArslaan <arhamoja@gmail.com>
2014-09-13 20:56:33 +0200

    Added real tweets display system

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( icons-000000.png icons-ffffff.png icons-000000@2x.png icons-ffffff@2x.png)
+Rails.application.config.assets.precompile += %w( home.js icons-000000.png icons-ffffff.png icons-000000@2x.png icons-ffffff@2x.png)
