42d56305756af63bdf998018589917e7b2da47ba
SirArslaan <arhamoja@gmail.com>
2014-09-15 12:01:50 +0200

    Refactorized some stuff

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,59 +6,46 @@ L.mapbox.accessToken = 'pk.eyJ1IjoiYnV6emxpZ2h0eWVhcjE4MiIsImEiOiJtQ1FQWXZNIn0.3
     var markers = new L.mapbox.featureLayer();
 
     $.get('http://localhost:3000/bad_points', function(data){
-      for(var i=0; i<data.length; i++){
-        var latitude = parseFloat(data[i].coordinates.split(',')[0]);
-        var longitude = parseFloat(data[i].coordinates.split(',')[1]);
-        var title = data[i].text;
-        var screen_name = data[i].screen_name;
-        var profile_pic = data[i].profile_pic
-        var marker = L.marker(new L.LatLng(latitude, longitude), {
-
-            title: title
-        });
-         marker.setIcon(L.mapbox.marker.icon({
-                    'marker-color': '#ff1111',
-                    'marker-size': 'large'
-                }));
-        marker.bindPopup(title);
-        markers.addLayer(marker);
-      }
-
-      map.addLayer(markers);
+      appendMarkers(data, 'red');
     });
 
     $.get('http://localhost:3000/good_points', function(data){
-      for(var i=0; i<data.length; i++){
-        var latitude = parseFloat(data[i].coordinates.split(',')[0]);
-        var longitude = parseFloat(data[i].coordinates.split(',')[1]);
-        var title = data[i].text;
-        var favourite_count = data[i].favourite_count;
-        var screen_name = data[i].screen_name;
-        var profile_pic = data[i].profile_pic
-        var marker = L.marker(new L.LatLng(latitude, longitude), {
-            title: title
-        });
-         marker.setIcon(L.mapbox.marker.icon({
-                    'marker-color': '#11ff11',
-                    'marker-size': 'large'
-                }));
-        map.legendControl.addLegend('<div><div class="upvote"><a href="#" class="upvote-link"></a><span class="vote-count">'+favourite_count+'</span></div><div class="screen_name">'+ screen_name +'</div><img id="tweet_img" src="'+ profile_pic +'" /><div class="tweet">'+title+'</div></div>');
-       
-        
-        marker.bindPopup(title);
-        markers.addLayer(marker);
-        var screen_name_div = document.getElementsByClassName("screen_name");
-        for (var i = 0; i < screen_name_div.length; i++) {
-            screen_name_div[i].addEventListener("click", myfuction, false); 
-        }
-        function myfuction(){
-           marker.bindPopup(title).openPopup();
-        }
-      }
-
-      map.addLayer(markers);
+      appendMarkers(data, 'green');
     });
 
     
-
-
+var appendMarkers = function(geoPoints, typeOfPoint){
+  for(var i=0; i<geoPoints.length; i++){
+    var latitude = parseFloat(geoPoints[i].coordinates.split(',')[0]);
+    var longitude = parseFloat(geoPoints[i].coordinates.split(',')[1]);
+    var title = geoPoints[i].text;
+    var marker = L.marker(new L.LatLng(latitude, longitude), {
+        title: title
+    });
+    if(typeOfPoint == 'red'){
+      marker.setIcon(L.mapbox.marker.icon({
+        'marker-color': '#ff1111',
+        'marker-size': 'large'
+      }));
+    }
+    else if(typeOfPoint == 'green'){
+      var favourite_count = geoPoints[i].favourite_count;
+      var screen_name = geoPoints[i].screen_name;
+      var profile_pic = geoPoints[i].profile_pic
+      marker.setIcon(L.mapbox.marker.icon({
+        'marker-color': '#11ff11',
+        'marker-size': 'large'
+      }));
+      map.legendControl.addLegend('<div><div class="upvote"><a href="#" class="upvote-link"></a><span class="vote-count">'+favourite_count+'</span></div><div class="screen_name">'+ screen_name +'</div><img id="tweet_img" src="'+ profile_pic +'" /><div class="tweet">'+title+'</div></div>');
+      var screen_name_div = document.getElementsByClassName("screen_name");
+      for (var i = 0; i < screen_name_div.length; i++) {
+          screen_name_div[i].addEventListener("click", function(){
+            marker.bindPopup(title).openPopup();
+          }, false); 
+      }
+    }
+    marker.bindPopup(title);
+    markers.addLayer(marker);
+  }
+  map.addLayer(markers);
+}
